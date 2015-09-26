fb1de458d55d14dea62e0d36cccec21db6185402
franklindeoliveira <franklindeoliveira@gmail.com>
2015-09-26 12:44:49 -0300

    Teste de consulta periodico do Jenkins.

--- src/test/java/br/com/jenkins/CalculadoraTest.java
+++ src/test/java/br/com/jenkins/CalculadoraTest.java
@@ -7,6 +7,7 @@ public class CalculadoraTest {
 	
 	@Test
 	public void somaDeDoisNumerosInteiros() {
+		System.out.println("Teste de consulta periodico do Jenkins.");
 		Calculadora calculadora = new Calculadora(1, 1);
 		Assert.assertEquals(calculadora.soma(), 2, Double.MIN_VALUE);
 	}
